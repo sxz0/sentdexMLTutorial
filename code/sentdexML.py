b9c91c77b4b0e8158974cc329145643540240bbe
Pedro Miguel Sanchez <pedro_weba@hotmail.com>
2018-09-22 02:13:59 +0200

    Final Video 6

--- code/sentdexML.py
+++ code/sentdexML.py
@@ -6,6 +6,7 @@ from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from matplotlib import style
+import pickle
 
 style.use('ggplot')
 
@@ -30,14 +31,17 @@ X=X[:-forecast_out]
 
 df.dropna(inplace=True)
 y=np.array(df['label'])
-y=np.array(df['label'])
-
 
 X_train,X_test,y_train,y_test=cross_validation.train_test_split(X,y,test_size=0.2)
 
-clf = LinearRegression()
+#clf = LinearRegression()
+#clf.fit(X_train,y_train)
+#with open('linearregression.pickle','wb') as f:
+#    pickle.dump(clf,f)
 
-clf.fit(X_train,y_train)
+pickle_in=open('linearregression.pickle','rb')
+clf=pickle.load(pickle_in)
+
 accuracy=clf.score(X_test,y_test)
 #print(accuracy)
 
